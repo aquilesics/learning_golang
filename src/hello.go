25b998809d52cc91b002d94be103f5630cc851da
aquilesics <aquiles.cout@gmail.com>
2021-08-17 03:00:33 -0300

    first commit

---
+++ src/hello.go
@@ -0,0 +1,11 @@
+package main 
+// Go program must start with it.
+// Packages are Go’s way of organizing and reusing code. There are two types of Go pro‐
+// grams: executables and libraries.
+
+import "fmt" 
+
+//printa o classico hello world :)
+func main(){
+	fmt.Println("Ola mundo")
+}
